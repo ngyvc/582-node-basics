2c5515d8561b1b5d825e0b802996c2808ef79d04
Peter Ng <ngy@vaniercollege.qc.ca>
2023-08-11 10:30:28 -0400

    add read example using callback

---
+++ fs-read-with-callback.mjs
@@ -0,0 +1,23 @@
+import { readFile, writeFile } from "fs";
+
+readFile("template.html", "utf-8", (err, data) => {
+  if (err) throw err;
+
+  console.log(data);
+
+  const replacement = {
+    title: "callback",
+    content: "Hello World!",
+  };
+
+  for (let key in replacement) {
+    data = data.replace(`{${key}}`, replacement[key]);
+  }
+
+  console.log(data);
+
+  writeFile("./static/index.html", data, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+});
